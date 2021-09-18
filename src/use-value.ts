13650ee8d7c7d08dbd7a4ae223bd3869c338c682
Welington Silva <welington.manoels@gmail.com>
2021-09-18 12:47:08 -0300

    fix: always return the current state from `value`

--- src/use-value.ts
+++ src/use-value.ts
@@ -11,16 +11,16 @@ function useValue<T>(initialValue: T | (() => T)) {
   const [state, setState] = useState(initialValue);
 
   /**
-   *
+   * Sets new value, if any is provided, and returns the current value.
    * @param  {T | (() => T)} [args] - If setting a new value, accepts same parameters as `React.SetStateAction<T>`
    * @return {T|void} returns `T` if called with no arguments, `void` otherwise.
    */
   function value(arg?: T | ((value: React.SetStateAction<T>) => T)) {
-    if (arg === undefined) {
-      return state;
+    if (arg !== undefined) {
+      setState(arg)
     }
 
-    return setState(arg);
+    return state;
   }
 
   return value;
